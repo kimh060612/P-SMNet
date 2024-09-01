50fc251fa6ca0c31529ada2937e194ff55b23a40
kimh060612 <kimh060612@naver.com>
2024-09-02 02:45:35 +0900

    .

--- SMNet/loss.py
+++ SMNet/loss.py
@@ -34,8 +34,8 @@ class AuxSemmapLoss(nn.Module):
 
     def forward(self, obj_gt, obj_pred, mask):
         mask = mask.float()
-        # print(obj_gt.shape, obj_pred.shape)
-        loss = self.loss(obj_pred, obj_gt) + self.beta * self.mse_loss(torch.max(obj_pred, dim=1).float(), obj_gt.float())
+        # print(obj_gt.shape, torch.argmax(obj_pred, dim=1, keepdim=True).shape)
+        loss = self.loss(obj_pred, obj_gt) + self.beta * self.mse_loss(torch.argmax(obj_pred, dim=1, keepdim=True).float(), obj_gt.float())
         loss = torch.mul(loss, mask)
         # -- mask is assumed to have a least one value
         loss = loss.sum()/mask.sum()
